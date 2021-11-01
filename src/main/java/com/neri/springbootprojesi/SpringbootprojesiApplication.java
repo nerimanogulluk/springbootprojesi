ac6253bfb00456694d3fcecf280aad800e4faee7
Neriman Oğulluk <nerimanogulluk@gmail.com>
2021-11-01 14:47:49 +0300

    .

---
+++ src/main/java/com/neri/springbootprojesi/SpringbootprojesiApplication.java
@@ -0,0 +1,13 @@
+package com.neri.springbootprojesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojesiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootprojesiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neri/springbootprojesi/SpringbootprojesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neri.springbootprojesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojesiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
